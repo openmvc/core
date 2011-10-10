b959926c3e56fae67b119d198dedf8bb1c3f7db5
Sergey Lukin <smartcoding@live.com>
2011-10-10 01:37:21 +0000

    replaced main defines

--- bootstrap.php
+++ bootstrap.php
@@ -1,43 +1,17 @@
 <?php
-/*** Turn DEBUG mode On/Off ***/
-define('DEV_MODE', 1);
-	error_reporting(DEV_MODE ? E_ALL : 0);
+
+	/**
+	 * Show Warnings & Errors if DEV_MODE define was set to (bool) TRUE
+	 */
+	defined('DEV_MODE') or define('DEV_MODE', 0);
+	error_reporting(DEV_MODE ? E_ALL & E_STRICT : 0);
 	ini_set('display_errors', DEV_MODE ? 1 : 0);
-
 	
-			####################### SET MAIN DEFINES ##########################
-				/*** Set WWW Paths ***/
-				define('WWW_PATH', 'http://' . str_replace('openmvc/index.php', '', $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']));
-				define('WWW_CSS_PATH', WWW_PATH . 'css/');
-				define('WWW_JS_PATH', WWW_PATH . 'js/');
-				define('WWW_IMAGES_PATH', WWW_PATH . 'images/');
-				
-				define('STATIC_DIR', dirname(dirname(__FILE__)) . '/static_files/');
-				
-				/*** Set OpenMVC absolute Paths ***/
-				define('OPENMVC_PATH', dirname(__FILE__). '/');
-				define('OPENMVC_SYSTEM_PATH', OPENMVC_PATH . 'system/');
-				define('OPENMVC_LIB_PATH', OPENMVC_PATH . 'lib/');
-				
-				/*** Set App absolute Paths ***/
-				define('APP_PATH', dirname(dirname(__FILE__)). '/app/');
-				define('CONTROLLERS_PATH', APP_PATH . 'controllers/');
-				define('VIEWS_PATH', APP_PATH . 'views/');
-				define('CONFIG_PATH', APP_PATH . 'config/');
-				define('MODELS_PATH', APP_PATH . 'models/');
-				define('ASSETS_PATH', APP_PATH . 'assets/');
-				
-				/*** Set Logs files paths ***/
-				define('LOGS_PATH', APP_PATH . 'logs/');
-				define('ERROR_LOG_FILE_PATH', LOGS_PATH . 'errors.log');
-				
-				/*** Set Error Codes ***/
-				define('ERRORCODE_TECHNICAL_DIFFICULTIES', 1001);
-			####################### SET MAIN DEFINES ##########################
-
+	/*** Set Error Codes ***/
+	define('ERRORCODE_TECHNICAL_DIFFICULTIES', 1001);
 
 	/*** Include Logger Class ***/
-	include_once (OPENMVC_SYSTEM_PATH . 'logger.class.php');
+	include_once (CORE_PATH . 'logger.class.php');
 	/*** Initialize Core Object (that manages all child objects/classes) ***/
 	include_once (OPENMVC_SYSTEM_PATH . 'core.class.php');
 	$Core = new Core;
@@ -72,7 +46,7 @@ define('DEV_MODE', 1);
 	try {
 	
 		/*** Using "Router" try to set "View" components and run proper "Controller" that will prepare and run output ***/
-		$Core->Router->loader();
+		Router::loader();
 	
 	} catch (Exception $e) {
 		
